1bb9730e24ec24bf8b407ca1c95acb36edb46a09
noohfaisal <noohfaisal@gmail.com>
2023-07-12 00:20:48 +0400

    updated to older app code

--- app.py
+++ app.py
@@ -1,41 +1,46 @@
 import streamlit as st
-from PIL import Image
-import numpy as np
 import tensorflow as tf
 import cv2
-class_names = ['daisy', 'dandelion', 'rose', 'sunflower', 'tulip']
+from PIL import Image, ImageOps
+import numpy as np
 
+class_names = ['daisy', 'dandelion', 'rose', 'sunflower', 'tulip']
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = tf.keras.models.load_model('my_model2.hdf5')
-    return model
+  model=tf.keras.models.load_model('my_model2.hdf5')
+  return model
+with st.spinner('Model is being loaded..'):
+  model=load_model()
+st.write("""
+         # Flower Classification
+         """
+         )
 
+file = st.file_uploader("Please upload an brain scan file", type=["jpg", "png"])
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
 def import_and_predict(image_data, model):
-    size = (180, 180)
-    image_data.thumbnail(size)
-    image_array = np.array(image_data)
-    img = cv2.cvtColor(image_array, cv2.COLOR_RGB2BGR)
-    img_reshape = img[np.newaxis, ...]
-    prediction = model.predict(img_reshape)
-    return prediction
 
-# Main app code
-st.title("Flower Classification")
+        size = (180,180)
+        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+        image = np.asarray(image)
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        #img_resize = (cv2.resize(img, dsize=(75, 75),    interpolation=cv2.INTER_CUBIC))/255.
 
-# Sidebar for input controls
-st.sidebar.title("Upload Image")
-file = st.sidebar.file_uploader("Please upload an image file", type=["jpg", "png"])
+        img_reshape = img[np.newaxis,...]
 
-# Load the model
-model = load_model()
+        prediction = model.predict(img_reshape)
 
-# Perform prediction if file is uploaded
-if file is not None:
+        return prediction
+if file is None:
+    st.text("Please upload an image file")
+else:
     image = Image.open(file)
     st.image(image, use_column_width=True)
     prediction = import_and_predict(image, model)
     score = tf.nn.softmax(prediction[0])
     st.write(prediction)
     st.write(score)
-    disp = f"This image most likely belongs to {class_names[np.argmax(score)]} with a {100 * np.max(score):.2f} percent confidence."
+    disp=f"This image most likely belongs to {class_names[np.argmax(score)]} with a {100 * np.max(score) :2f} percent confidence."
+
     st.write(disp)
